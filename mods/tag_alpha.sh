769b9820042e420a7ef7e18af2a1c9d2f04efb12
wukong <sec-wukong@outlook.com>
2025-08-16 15:58:59 +0800

    update ver adm

--- mods/tag_alpha.sh
+++
@@ -1,6 +0,0 @@
-alpha_num=1 
-while git rev-parse "v${VERSION}-alpha.${alpha_num}" >/dev/null 2>&1; do 
-    alpha_num=$((alpha_num+1)); 
-done 
-git tag "v${VERSION}-alpha.${alpha_num}" 
-git push --tags
--- mods/tag_rc.sh
+++
@@ -1,6 +0,0 @@
-rc_num=1 
-while git rev-parse "v${VERSION}-rc.${rc_num}" >/dev/null 2>&1; do 
-    rc_num=$((rc_num+1)); 
-done 
-git tag "v${VERSION}-rc.${rc_num}" 
-git push --tags
